8e18874569dabd5b1d06115563ca1b782a360ab7
jairanjankumar <jairanjankumar@yahoo.co.in>
2020-01-17 00:54:25 +0530

    First Commit

---
+++ src/main/java/com/github/jairanjankumar/springboot/example/basics/SpringbootExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.github.jairanjankumar.springboot.example.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootExamplesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/jairanjankumar/springboot/example/basics/SpringbootExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.jairanjankumar.springboot.example.basics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
